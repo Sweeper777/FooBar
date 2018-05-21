232118c19266598feb0de1e2920cedcb7383d7a0
Sweeper777 <sumulang@gmail.com>
2018-05-21 08:44:10 +0100

    added RuleBuilder

---
+++ FooBar/RuleBuilder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace FooBar {
+    public class RuleBuilder {
+        RuleBuilder(string output) { this.output = output; }
+
+        string output;
+
+        public static RuleBuilder Output(object obj) => new RuleBuilder(obj.ToString());
+
+        public IRule<T> IfAndOnlyIf<T>(Predicate<T> predicate) => new PredicateRule<T>(output, true, predicate);
+
+        public IRule<T> If<T>(Predicate<T> predicate) => new PredicateRule<T>(output, false, predicate);
+    }
+}
