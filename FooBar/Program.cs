933d31c8194a5c42cd362475f71567261cd2b83e
Sweeper777 <sumulang@gmail.com>
2018-05-21 08:41:42 +0100

    initial commit

---
+++ FooBar/Program.cs
@@ -0,0 +1,9 @@
+using static FooBar.FluentInterface;
+using static FooBar.RuleBuilder;
+
+namespace FooBar {
+    class MainClass {
+        public static void Main(string[] args) {
+        }
+    }
+}
