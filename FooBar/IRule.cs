49b7b354b3a409964ca8af8ca069c947bb67a93f
Sweeper777 <sumulang@gmail.com>
2018-05-25 09:47:23 +0100

    replace IfAndOnlyIf with Rank

--- FooBar/IRule.cs
+++ FooBar/IRule.cs
@@ -2,21 +2,23 @@
 namespace FooBar {
     public interface IRule<in TInput> {
         string Output { get; }
-        bool IfAndOnlyIf { get; }
+        int Rank { get; }
 
         bool Test(TInput number);
     }
 
     public class PredicateRule<TInput>: IRule<TInput> {
         public string Output { get; }
-        public bool IfAndOnlyIf { get; }
+
+        public int Rank { get; }
+
         Predicate<TInput> predicate;
 
         public bool Test(TInput input) => predicate(input);
 
-        public PredicateRule(string output, bool iff, Predicate<TInput> predicate) {
+        public PredicateRule(int rank, string output, Predicate<TInput> predicate) {
             Output = output;
-            IfAndOnlyIf = iff;
+            Rank = rank;
             this.predicate = predicate;
         }
     }
