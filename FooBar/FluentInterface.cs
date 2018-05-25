ec134c4a33678d0d5819f3548b74d14d0d483545
Sweeper777 <sumulang@gmail.com>
2018-05-25 08:44:04 +0100

    added FluentInterface

---
+++ FooBar/FluentInterface.cs
@@ -0,0 +1,6 @@
+﻿using System;
+namespace FooBar {
+    public static class FluentInterface {
+        public static FooBarRunner RunFooBar(int from, int to, int step = 1) => new FooBarRunner(from, to, step);
+    }
+}
