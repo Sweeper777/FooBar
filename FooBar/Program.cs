b74bfd88128d40373ee2a730429b9e8014bdeee4
Sweeper777 <sumulang@gmail.com>
2018-05-25 08:44:15 +0100

    Did a simple test run

--- FooBar/Program.cs
+++ FooBar/Program.cs
@@ -1,9 +1,14 @@
-using static FooBar.FluentInterface;
+﻿using static FooBar.FluentInterface;
 using static FooBar.RuleBuilder;
 
 namespace FooBar {
     class MainClass {
         public static void Main(string[] args) {
+            RunFooBar(from: 1, to: 100).ApplyRules(
+                Output("Foo").IfAndOnlyIf(TheThing.IsDivisibleBy(3)),
+                Output("Bar").IfAndOnlyIf(TheThing.IsDivisibleBy(4)),
+                Output("FooBar").If(TheThing.IsDivisibleBy(3).And(TheThing.IsDivisibleBy(4)))
+            );
         }
     }
 }
