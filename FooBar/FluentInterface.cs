efa9b2cbf700795e0683d6476d874ccb893c68ec
Sweeper777 <sumulang@gmail.com>
2018-05-26 19:45:50 +0100

    added Output extension method to int

--- FooBar/FluentInterface.cs
+++ FooBar/FluentInterface.cs
@@ -2,5 +2,12 @@
 namespace FooBar {
     public static class FluentInterface {
         public static FooBarRunner RunFooBar(int from, int to, int step = 1) => new FooBarRunner(from, to, step);
+
+        public static RuleBuilder Output(this int rank, object obj) {
+            var builder = new RuleBuilder();
+            builder.SetOutput(obj);
+            builder.SetRank(rank);
+            return builder;
+        }
     }
 }
