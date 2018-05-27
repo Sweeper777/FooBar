e0249d332392add04263df5da613377dfa9d8c57
Sweeper777 <sumulang@gmail.com>
2018-05-27 11:24:55 +0100

    added TestRuleBuilder

--- FooBarTest/Test.cs
+++ FooBarTest/Test.cs
@@ -14,5 +14,14 @@ namespace FooBar.Test {
             Assert.IsFalse(predicateRule.Test(1));
         }
 
+        [Test]
+        public void TestRuleBuilder() {
+            var builder = new RuleBuilder();
+            var rule = builder.SetOutput("output").SetRank(1).If<int>(x => x == 0);
+            Assert.AreEqual(rule.Rank, 1);
+            Assert.AreEqual(rule.Output, "output");
+            Assert.IsTrue(rule.Test(0));
+            Assert.IsFalse(rule.Test(1));
+        }
     }
 }
