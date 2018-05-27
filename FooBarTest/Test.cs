016feec1ccba5cac0f5b94e2553f6ba29cf5f963
Sweeper777 <sumulang@gmail.com>
2018-05-27 11:24:37 +0100

    added TestPredicateRule

--- FooBarTest/Test.cs
+++ FooBarTest/Test.cs
@@ -5,5 +5,14 @@ using System.IO;
 namespace FooBar.Test {
     [TestFixture]
     public class FooBarTest {
+        [Test]
+        public void TestPredicateRule() {
+            var predicateRule = new PredicateRule<int>(1, "output", x => x == 0);
+            Assert.AreEqual(predicateRule.Rank, 1);
+            Assert.AreEqual(predicateRule.Output, "output");
+            Assert.IsTrue(predicateRule.Test(0));
+            Assert.IsFalse(predicateRule.Test(1));
+        }
+
     }
 }
