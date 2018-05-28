78d40467af80607bbe5d7980b96aaa634d39d641
Sweeper777 <sumulang@gmail.com>
2018-05-28 08:52:57 +0100

    overrode exception message

--- FooBar/ContradictingRulesException.cs
+++ FooBar/ContradictingRulesException.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 namespace FooBar {
@@ -9,5 +9,11 @@ namespace FooBar {
             this.rules = rules;
         }
 
+        public override string Message {
+            get {
+                var rulesString = string.Join(", ", rules.Select(x => x.ToString()));
+                return $"{rules} contradict each other";
+            }
+        }
     }
 }
